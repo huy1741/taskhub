8f06903c389a366571ae837e99ccf555827f13a5
huy1741 <huy1741@gmail.com>
2020-10-31 01:13:56 +0200

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import CustomerCard from './CustomerCard';
 
-
 const UserList = ({info}) =>{
     const userDetails = info.map(user => 
        <CustomerCard key={user.id} data={user}/>
